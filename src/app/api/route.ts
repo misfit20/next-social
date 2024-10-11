263f1f2a2763142bdd7a36f7bde3ec3e79537fb7
misfit20 <tchallablackpanther961@gmail.com>
2024-10-11 11:28:57 +1000

    added prisma orm and supabase database hosting

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,4 @@
 import NextAuth from 'next-auth/next';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
 import { authOptions } from '../../../lib/authOptions';
 
 const handler = NextAuth(authOptions)
