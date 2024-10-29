6c0ac3f82947e09efe08157df189b7e1fe334724
misfit20 <tchallablackpanther961@gmail.com>
2024-10-29 17:27:22 +1000

    Updated globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,45 @@
-
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import "./globals.css";
 import SessionWrapper from "../components/SessionWrapper";
-const inter = Inter({ subsets: ["latin"] });
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
-import MobileMenu from "@/components/MobileMenu";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kibung Reviews",
   description: "Discover and rate businesses both small and large.",
   icons: {
-    icon: ['/favicon.ico?v=4'],
-    apple: ['/apple-touch-icon.png?v=4'],
-    shortcut: ['/apple-touch-icon.png?']
-  }
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png?"],
+  },
 };
 
-export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    
-      
-    
-      <html lang="en">
-        <body className={inter.className}>
+    <html lang="en">
+      <body className={inter.className}>
         <SessionWrapper>
-          
-      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem
-            disableTransitionOnChange>
-          <div className="w-full  px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-            <Navbar />
-          </div>
-          <div className="w-full px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 space-y-5">
-          {children}
-          </div>
+          <NextThemesProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="w-full px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+              <Navbar />
+              <main className="space-y-5">{children}</main>
+            </div>
           </NextThemesProvider>
-          </SessionWrapper>      
-        </body>
-      </html>
-    
+        </SessionWrapper>
+      </body>
+    </html>
   );
 }
