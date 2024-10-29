3895324757b4754358ee0c5f4d865379eff8f111
misfit20 <tchallablackpanther961@gmail.com>
2024-10-29 21:44:20 +1000

    Added shacn UI again!

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}, // Add this line to include autoprefixer
-  },
-};
-
-export default config;
